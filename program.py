0d7f28971728a7fd4e7d7d69b928b62b4d6ae561
lailacode <106543871+lailacode@users.noreply.github.com>
2022-05-30 20:08:01 +0200

    Add files via upload

---
+++ program.py
@@ -0,0 +1,6 @@
+num_one = int(input('First number: \n'))       # hämta första talet från användaren
+num_two = int(input('Second number: \n'))      # hämta andra talet
+
+import modules                                 # importera modules filen
+modules.number_list(num_one, num_two)
+modules.guess_number()
